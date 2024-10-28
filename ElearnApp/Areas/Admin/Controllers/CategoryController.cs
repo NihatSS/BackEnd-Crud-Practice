de7f675361ac5d85814a7a6b2a376e9105f49091
User <nihatss@code.edu.az>
2024-10-28 23:06:17 +0400

    crud practice

---
+++ ElearnApp/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using ElearnApp.Data;
+using ElearnApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElearnApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Category> categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            return View(category);
+        }
+    }
+}
